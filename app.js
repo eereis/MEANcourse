ec42ed2756028562a228beafe246317622687015
eki <eerik.reis@gmail.com>
2018-09-14 14:19:40 +0300

    added about

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var eerik = require('./routes/eerik.js');
+var eerik = require('./routes/eerik');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/eerik', eerik);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
