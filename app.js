28eabee5e5a2bda1e40f49ab7e51b7286103e580
eki <eerik.reis@gmail.com>
2018-09-14 14:22:20 +0300

    ...

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var eerik = require('./routes/eerik');
-var about = require('./routes/about');
+var eerikRouter = require('./routes/eerik');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -23,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/eerik', eerik);
-app.use('/about', about);
+app.use('/eerik', eerikRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
